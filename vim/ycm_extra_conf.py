f830b5dadaa6324dae26dfa31a17d8948d4f51e9
Anish Sevekari <anish.sevekari@gmail.com>
2018-04-22 07:19:21 -0400

    Reconfiguring ycm_extra_conf.py

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -31,10 +31,8 @@
 import os
 import ycm_core
 
-# These are the compilation flags that will be used in case there's no
-# compilation database set (by default, one is not set).
-# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
+# Compilatiion Flags
 '-Wall',
 '-Wextra',
 '-Werror',
@@ -42,47 +40,33 @@ flags = [
 '-Wno-variadic-macros',
 '-fexceptions',
 '-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=c++11',
+'-std=c++0x',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
+# Following lines mainly includes paths of headers that need to be added
+# for the compilation of project.
+'-isystem', '/usr/include/c++/7',
+'-isystem', '/usr/include/x86_64-linux-gnu/c++/7',
+'-isystem', '/usr/include/c++/7/backward',
+'-isystem', '/usr/lib/gcc/x86_64-linux-gnu/7/include',
+'-isystem', '/usr/local/include',
+'-isystem', '/usr/lib/gcc/x86_64-linux-gnu/7/include-fixed',
+'-isystem', '/usr/include/x86_64-linux-gnu',
+'-isystem', '/usr/include',
 ]
 
 
+# Some automation from default .ycm_extra_conf.py in YouCompleteMe
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
 # more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
@@ -90,9 +74,6 @@ flags = [
 # You can get CMake to generate this file for you by adding:
 #   set( CMAKE_EXPORT_COMPILE_COMMANDS 1 )
 # to your CMakeLists.txt file.
-#
-# Most projects will NOT need to set this to anything; you can just change the
-# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
 if os.path.exists( compilation_database_folder ):
@@ -170,13 +151,6 @@ def FlagsForFile( filename, **kwargs ):
       compilation_info.compiler_flags_,
       compilation_info.compiler_working_dir_ )
 
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-    try:
-      final_flags.remove( '-stdlib=libc++' )
-    except ValueError:
-      pass
   else:
     relative_to = DirectoryOfThisScript()
     final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
