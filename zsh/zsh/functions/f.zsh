5ba80d9c66e3ecbe6d014253d82e7b5cf17c0a73
Anish Sevekari <anish.sevekari@gmail.com>
2018-11-07 20:51:20 -0500

    minor snippet and latex changes

---
+++ zsh/zsh/functions/f.zsh
@@ -0,0 +1,10 @@
+# Author: Anish Sevekari
+# Last Edited: Tue 09 Oct 2018 02:19:25 PM EDT
+# Function to easily find files in subtree.
+# Uses find function, just strips some syntax.
+
+# TODO: actually impliment
+# TODO: List options if there are multiple
+
+
+
