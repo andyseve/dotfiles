99e96da3212c39bf4bb71f6aef4fb9d08e74fa16
Anish Sevekari <anish.sevekari@gmail.com>
2019-04-20 23:07:59 +0000

    minor bug fix

--- vpn/routing.sh
+++ vpn/routing.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 VPNIF="tun0"
 VPNUSER="rtorrent"
-GATEWAYIP='ifconfig $VPNIF | egrep -i '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n1'
+GATEWAYIP='ifconfig $VPNIF | egrep -i '([0-9]{1,3}\.){3}[0-9]{1,3}' | egrep -v '255|(127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})' | tail -n1'
 echo $GATEWAYIP
 if [[ 'ip rule list | grep -c 0x1' == 0 ]]; then
 	ip rule add from all fwmark 0x1 lookup $VPNUSER
