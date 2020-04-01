9e0cd93f9b8df6fdd001741841d795c2624c9cdf
Anish Sevekari <anish.sevekari@gmail.com>
2020-04-01 01:48:55 -0400

    nixos with majority of config

---
+++ nixos/desktop.nix
@@ -0,0 +1,51 @@
+{ configs, pkgs, ... }:
+
+{
+  # Xserver settings
+	services.xserver = {
+		enable = true;
+		layout = "us";
+		# xkbOptions = "eurosign:e"
+		
+		# Touchpad
+		libinput.enable = true;
+
+    # Desktop
+		displayManager.slim.enable = true;
+		windowManager.xmonad = {
+			enable = true;
+			enableContribAndExtras = true;
+			extraPackages = haskellPackages: [
+				haskellPackages.xmonad-contrib
+				haskellPackages.xmonad-extras
+				haskellPackages.xmonad
+			];
+		};
+		windowManager.default = "xmonad";
+	};
+
+	environment.systemPackages = with pkgs; [	
+		# WindowManager Core
+		haskellPackages.xmobar
+		rofi
+		dunst
+		feh
+		compton
+		xdotool xorg.xmodmap xorg.xrandr
+		scrot
+
+		# Desktop Programs
+		# modularize these, should only be installed if xserver is set
+		firefox google-chrome vivaldi
+		vlc
+		zathura
+		vimHugeX emacs
+		libreoffice
+		gimp
+		alacritty rxvt_unicode-with-plugins
+		steam
+		discord zoom-us
+		deluge
+		google-play-music-desktop-player
+	];
+}
