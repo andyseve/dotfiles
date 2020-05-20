00104dde128f64151e75222463d9e2ff0bdf4f14
Anish Sevekari <anish.sevekari@gmail.com>
2020-05-20 18:30:20 -0400

    picom updates, glx is still fucked with nvidia

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -2,10 +2,14 @@
 
 {
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
 
   environment.systemPackages = with pkgs; [
-    cudatoolkit_10
+    cudatoolkit
     python3Packages.tensorflow
     python3Packages.pytorch
+    linuxPackages.nvidia_x11
+    vulkan-loader vulkan-validation-layers vulkan-tools
   ];
 }
