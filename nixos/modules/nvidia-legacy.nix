c7d0b7e6181c38a82626c26ab9844eadf222af66
Anish Sevekari <anish.sevekari@gmail.com>
2020-10-13 15:15:36 -0400

    fixing minor errors

--- nixos/modules/nvidia-legacy.nix
+++ nixos/modules/nvidia-legacy.nix
@@ -6,6 +6,10 @@
   hardware.opengl.driSupport32Bit = true;
 
   environment.systemPackages = with pkgs; [
+	  cudatoolkit
+		python3Packages.tensorflow
+		python3Packages.pytorch
     linuxPackages.nvidia_x11_legacy390
+		vulkan-loader vulkan-validation-layers vulkan-tools
   ];
 }
