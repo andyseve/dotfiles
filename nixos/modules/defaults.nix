9394d3650aedd06b3f84a49c33886d7bca57e1b4
Anish Sevekari <anish.sevekari@gmail.com>
2020-12-27 13:06:51 -0500

    printing update

--- nixos/modules/defaults.nix
+++ nixos/modules/defaults.nix
@@ -142,6 +142,11 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = with pkgs; [
+    gutenprint
+    hplip
+    hplipWithPlugin
+  ];
 
   # Enable sound.
   sound.enable = true;
