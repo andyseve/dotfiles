a51431ff69eaff85d06311a4286bcd3e9204103f
Anish Sevekari <anish.sevekari@gmail.com>
2021-11-22 20:53:55 -0500

    fixed nvidia-legacy not working

--- nixos/modules/nvidia-legacy.nix
+++ nixos/modules/nvidia-legacy.nix
@@ -4,18 +4,22 @@
 
 {
   #services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
-  services.xserver.videoDrivers = lib.mkDefault [ "nouveau" "modesetting" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+  #services.xserver.videoDrivers = lib.mkDefault [ "nouveau" "modesetting" ];
 
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
-
-  hardware.nvidia.prime = {
-    sync.enable = lib.mkDefault true;
-    nvidiaBusId = "PCI:4:0:0";
-    intelBusId  = "PCI:0:2:0";
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_390;
+    prime = {
+      sync.enable = lib.mkDefault true;
+      nvidiaBusId = "PCI:4:0:0";
+      intelBusId  = "PCI:0:2:0";
+    };
+    modesetting.enable = false;
   };
 
   hardware.opengl = {
     enable = true;
+    driSupport = true;
     driSupport32Bit = true;
     extraPackages = [ pkgs.vaapiIntel ];
   };
@@ -37,16 +41,10 @@
     #Option Xinerama "ON"
   #'';
 
+  # Intel driver settings
   #services.xserver.videoDrivers = [ "intel" ];
   #services.xserver.deviceSection = ''
     #Option "DRI" "2"
     #Option "TearFree" "true"
   #'';
-
-
-  #environment.systemPackages = with pkgs; [
-    #cudatoolkit
-    #python3Packages.tensorflow
-    #python3Packages.pytorch
-  #];
 }
