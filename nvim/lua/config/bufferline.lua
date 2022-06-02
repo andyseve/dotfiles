53fd4a6741b4a886b2bd2b2427f9ff3d17366c88
Anish Sevekari <anish.sevekari@gmail.com>
2022-06-02 18:20:33 -0400

    bufferline settings update

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -1,5 +1,5 @@
 -- Author: Anish Sevekari
--- Last Modified: Sat Jan 15 16:41:47 2022
+-- Last Modified: Thu 02 Jun 2022 06:20:09 PM EDT
 -- bufferline settings
 
 local present, bufferline = pcall(require, 'bufferline')
@@ -10,7 +10,7 @@ end
 
 bufferline.setup {
   options = {
-		numbers = "both", --numbers = "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+		numbers = "none", --numbers = "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
     close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
     right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
@@ -25,42 +25,36 @@ bufferline.setup {
     close_icon = '',
     left_trunc_marker = '',
     right_trunc_marker = '',
-    --- name_formatter can be used to change the buffer's label in the bufferline.
-    --- Please note some names can/will break the
-    --- bufferline so use this at your discretion knowing that it has
-    --- some limitations that will *NOT* be fixed.
+
+    -- name_formatter can be used to change the buffer's label in the bufferline.
+    -- Please note some names can/will break the
+    -- bufferline so use this at your discretion knowing that it has
+    -- some limitations that will *NOT* be fixed.
     name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
       -- remove extension from markdown files for example
-      if buf.name:match('%.md') then
+      if buf.name:match('%.md') or buf.name:match('%.txt') or buf.name:match('%.tex') then
         return vim.fn.fnamemodify(buf.name, ':t:r')
       end
     end,
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 18,
-    diagnostics = "nvim_lsp",
-    --diagnostics = false | "nvim_lsp" | "coc",
+    diagnostics = "nvim_lsp",	--diagnostics = false | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       return "("..count..")"
     end,
+
     -- NOTE: this will be called a lot so don't do any heavy processing here
     custom_filter = function(buf_number)
+			buf_filetype = vim.bo[buf_number].filetype
       -- filter out filetypes you don't want to see
-      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        return true
-      end
-      -- filter out by buffer name
-      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        return true
-      end
-      -- filter out based on arbitrary rules
-      -- e.g. filter out vim wiki buffer from tabline in your work repo
-      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+      if buf_filetype ~= "NvimTree" and buf_filetype ~= "qf" then
         return true
       end
     end,
-    offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left" }},
+
+    offsets = {{filetype = "NvimTree", text = "File Explorer", highlight="Directory", text_align = "left" }},
     show_buffer_icons = true, -- disable filetype icons for buffers
     show_buffer_close_icons = true,
     show_close_icon = true,
@@ -68,10 +62,10 @@ bufferline.setup {
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-		separator_style = "thick",
-    --separator_style = "slant" | "thick" | "thin" | { 'any', 'any' },
+		separator_style = "thick", --separator_style = "slant" | "thick" | "thin" | { 'any', 'any' },
     enforce_regular_tabs = false,
     always_show_bufferline = true,
+
 		sort_by = 'relative_directory',
     --sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
       -- add custom logic
