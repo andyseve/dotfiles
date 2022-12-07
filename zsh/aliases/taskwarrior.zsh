7c05da46167b8e0813357194d7beb1db5e4eb2f2
Anish Sevekari <anish.sevekari@gmail.com>
2022-12-07 13:11:42 -0500

    adding personal task command

--- zsh/aliases/taskwarrior.zsh
+++ zsh/aliases/taskwarrior.zsh
@@ -1,4 +1,6 @@
-alias taskr="task project:research"
 alias taskn="task +next"
+
+alias taskr="task project:research"
 alias taskt="task project:ta"
 alias tasks="task project:setup"
+alias taskp="task project:personal"
